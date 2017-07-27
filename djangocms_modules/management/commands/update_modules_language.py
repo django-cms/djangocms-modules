05dd58332975dc0dad07939de80bc5acc365c07e
Paulo <commonzenpython@gmail.com>
2017-07-27 05:30:27 -0400

    Added update_modules_language command

---
+++ djangocms_modules/management/commands/update_modules_language.py
@@ -0,0 +1,21 @@
+from __future__ import unicode_literals
+
+from django.conf import settings
+from django.core.management.base import BaseCommand
+
+from djangocms_modules.models import Category
+
+
+class Command(BaseCommand):
+    help = 'Updates the language for each plugin in a module category'
+
+    def handle(self, *args, **options):
+        count = 0
+        language = settings.LANGUAGE_CODE
+        categories = Category.objects.select_related('modules')
+
+        for category in categories.iterator():
+            count += 1
+            category.modules.update(language=language)
+
+        self.stdout.write('Successfully updated "%d" module categories.' % count)
